b481b470a8f7921e349537f6e3998119113be484
Luiz Mo <luizmorey@gmail.com>
2019-12-07 12:04:09 -0700

    space cheese

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,57 @@
 class LocationsController < ApplicationController
+  before_action :set_trip
+  before_action :set_location, only: [:show, :update, :edit, :destroy]
+  
   def index
+    @locations = @trip.locations
   end
 
   def show
   end
 
   def new
+    @location = @trip.locations.new
+    render partial: 'form'
+  end
+
+  def create
+    @location = @trip.locations.new(location_params)
+    if @location.save
+      redirect_to [@trip, @location]
+    else
+      render :new
+    end
   end
 
   def edit
+    render partial: 'form'
   end
+
+  def update
+    if @location.update(location_params)
+      redirect_to [@trip, @location]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @location.destroy
+    redirect_to trip_locations_path(@trip)
+  end
+
+  private 
+    def set_trip
+      @trip = current_user.trips.find(params[:trip_id])
+    end
+
+    def set_location
+      @location = Location.find(params[:id])
+    end
+
+    def location_params
+      params.require(:location).permit(:name, :days)
+    end
+
+    
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,51 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: [:show, :update, :edit, :destroy]
+  
   def index
+    @trips = current_user.trips
   end
 
   def show
   end
 
   def new
+    @trip = Trip.new
+    render partial: 'form'
+  end
+
+  def create
+    @trip = current_user.trips.new(trip_params)
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end
   end
 
   def edit
+    render partial: 'form'
   end
+
+  def update
+    if @trip.update(trip_params)
+      redirect_to trips_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @trip.destroy
+    redirect_to trips_path
+  end
+
+  private 
+    def trip_params
+      params.require(:trip).permit(:name, :start_date, :end_date)
+    end
+
+    def set_trip
+      @trip = current_user.trips.find(params[:id])
+    end
+
 end
